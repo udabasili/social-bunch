2f78c3326e156a1a6d3a37a204f879893c6c0f92
udabasili <udbasili@yahoo.com>
2020-01-15 08:47:05 -0400

    update

--- controllers/imageUpload.js
+++ controllers/imageUpload.js
@@ -1,13 +1,13 @@
 const util = require('util')
 const Cloud = require('@google-cloud/storage')
 const path = require('path')
-
+let serviceKey;
 if (process.env.NODE_ENV === "production"){
-  let serviceKey = process.env.CREDS
+  serviceKey = process.env.CREDS
   serviceKey = JSON.parse(serviceKey)
 }
 else{
-  let serviceKey = path.join(__dirname, '../utils/simply-chart-b0f4e69d71d8.json')
+  serviceKey = path.join(__dirname, '../utils/simply-chart-b0f4e69d71d8.json')
 
 }
 
