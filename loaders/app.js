2a41198ac23571fb5815ca3532bee3bc93005c17
udabasili <udbasili@yahoo.com>
2020-05-02 18:51:51 -0300

    project restructuring and testing

--- loaders/app.js
+++ loaders/app.js
@@ -19,9 +19,9 @@ const path = require('path');
 /**STATIC FILES */  
     if (process.env.NODE_ENV === 'production') {
         app.use(express.static(__dirname));
-        app.use(express.static(path.join(__dirname,'/client/build')))
+        app.use(express.static(path.join(__dirname,'../client/build')))
         app.get('/*', (req, res)=>{
-            res.sendFile(path.join(__dirname, '/client/build/index.html'))
+            res.sendFile(path.join(__dirname, '../client/build/index.html'))
         })
     }
 
