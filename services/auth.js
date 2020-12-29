8549c8f5b8d0e5869ae5a2bf72d400be3a2c2f9a
udabasili <udbasili@yahoo.com>
2020-12-29 02:28:29 -0400

    update

--- services/auth.js
+++ services/auth.js
@@ -37,8 +37,12 @@ class AuthService {
                 userImage: 'https://images.unsplash.com/photo-1582266255765-fa5cf1a1d501?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80',
                 isAdmin: true,
                 email: 'admin@yahoo.com',
-                password: '11111111'
-
+                password: '11111111',
+                bio: 'As a Field Sales Manager with over 8 years of experience driving market share growth in designated territories, I have mastered the ins and outs of pharmaceutical sales and territorial prospecting.After honing and executing these specialties to reach numerous company goals, I was honored with an invitation to join the National Marketing Council.Now, I spend the majority of my time brainstorming sales strategies and connecting with other industry professionals who are interested in talking shop',
+                dateOfBirth: Date.now(),
+                phoneNumber: '6172223333',
+                occupation: 'Field Sales Manager',
+                location: 'California' 
             })
             await adminUser.encryptPassword()
 
